00b111e6306904e6460f3653a3cad92d89e85efb
Rikie Patrick <rikiepatrick@gmail.com>
2023-08-15 11:33:51 +0100

    initial commmit

--- app.py
+++ app.py
@@ -1,24 +1,69 @@
+from example_routes import apply_example_routes
 import os
 from flask import Flask, request
+from lib.database_connection import get_flask_database_connection
+from lib.album_repository import AlbumRepository
+from lib.album import Album
+from lib.artist import Artist
+from lib.artist_repository import ArtistRepository
+
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
 
-# == Example Code Below ==
+# GET /albums
+# Returns nothing
+#
 
-# GET /emoji
-# Returns a emojiy face
+
+@app.route('/albums', methods=['POST'])
+def post_album():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    title = request.form['title']
+    release_year = request.form['release_year']
+    artist_id = request.form['artist_id']
+    repository.create(Album(None, title, release_year, artist_id))
+    return 'Album Created'
+
+# POST /albums
+# Returns nothing
 # Try it:
-#   ; curl http://localhost:5000/emoji
-@app.route('/emoji', methods=['GET'])
-def get_emoji():
-    return ":)"
+#   ; curl http://localhost:5000/albums
+
+
+@app.route('/albums', methods=['GET'])
+def get_album():
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+    albums = repository.all()
+    titles = [album.title for album in albums]
+    return ', '.join(titles)
+
+
+@app.route('/artists', methods=['GET'])
+def get_artists():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    artists = repository.all()
+    artist_names = [artist.name for artist in artists]
+    return ', '.join(artist_names)
+
+
+@app.route('/artists', methods=['POST'])
+def post_artist():
+    connection = get_flask_database_connection(app)
+    repository = ArtistRepository(connection)
+    name = request.form['name']
+    genre = request.form['genre']
+    repository.create(Artist(None, name, genre))
+    return ''
+
 
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
-from example_routes import apply_example_routes
 apply_example_routes(app)
 
 # == End Example Code ==
@@ -28,4 +73,3 @@ apply_example_routes(app)
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
-
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,11 +3,58 @@
 # === Example Code Below ===
 
 """
-GET /emoji
+POST /albums
+Body Params:
+    title=Voyage
+    release_year=2022
+    artist_id=2
+Returns:
+    Nothing
+Side effects:
+    Adds album to db
 """
-def test_get_emoji(web_client):
-    response = web_client.get("/emoji")
+
+
+def test_post_album(db_connection, web_client):
+    db_connection.seed('seeds/music_web_app.sql')
+    response = web_client.post(
+        "/albums", data={'title': 'Test Album', 'release_year': 2000, 'artist_id': 1})
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == ":)"
+    assert web_client.get("/albums").data.decode(
+        'utf-8') == 'Group Therapy, Whats The Story Morning Glory, Test Album'
+
+
+def test_get_albums(db_connection, web_client):
+    db_connection.seed('seeds/music_web_app.sql')
+    response = web_client.get("/albums")
+    assert response.status_code == 200
+    assert response.data.decode(
+        'utf-8') == 'Group Therapy, Whats The Story Morning Glory'
+
+
+'''
+Get /artists
+Returns list of artists in a string
+Response Status 200
+e.g. Taylor Swift, Above and Beyond
+'''
+
+
+def test_get_artists(db_connection, web_client):
+    db_connection.seed('seeds/music_web_app.sql')
+    response = web_client.get('/artists')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Taylor Swift, Above and Beyond'
+
+
+def test_post_artist(db_connection, web_client):
+    db_connection.seed('seeds/music_web_app.sql')
+    response = web_client.post(
+        '/artists', data={'name': 'Wild Nothing', 'genre': 'Indie'})
+    artists = web_client.get('/artists')
+    assert response.status_code == 200
+    assert artists.data.decode(
+        'utf-8') == 'Taylor Swift, Above and Beyond, Wild Nothing'
+
 
 # === End Example Code ===
